c99a6cd3ff16d6d49efd3ea42a95d8f889f27b56
José García Izaguirre <joseizaguirreg34@gmail.com>
2023-10-28 20:20:23 -0600

    Se agregan los archivos principales

---
+++ src/proyectoIII/DriverApp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package proyectoIII;
+
+/**
+ * 
+ */
+public class DriverApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/proyectoIII/EmployeeApp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package proyectoIII;
+
+/**
+ * 
+ */
+public class EmployeeApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
